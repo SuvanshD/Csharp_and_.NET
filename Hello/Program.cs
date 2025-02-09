af0b813769c7b68624bf64b17b1cc186cc6afdba
Suvansh <suvanshd27@gmail.com>
2025-02-10 03:45:49 +0530

    C# Basics done

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -8,6 +8,16 @@ namespace Hello
 {
     internal class Program
     {
+        static void Greet(string name)
+        {
+            Console.WriteLine("Hello " + name); 
+        }
+
+        static float Average(int a, int b, int c)
+        {
+            return (a+b+c) / 3;
+        }
+
         static void Main(string[] args)
         {
             // This is a one line comment
@@ -49,13 +59,70 @@ namespace Hello
             //Console.WriteLine(num);
 
 
-            Console.Write("Enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hey "+name);
+            //Console.Write("Enter your name: ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine("Hey "+name);
 
-            Console.Write("Enter a number: ");
-            string num = Console.ReadLine();
-            Console.WriteLine("Your num + 4 is: " + (Convert.ToInt32(num) + 4));
+            //Console.Write("Enter a number: ");
+            //string num = Console.ReadLine();
+            //Console.WriteLine("Your num + 4 is: " + (Convert.ToInt32(num) + 4));
+
+            //int a = 4;
+            //int b = 5;
+
+            //Console.WriteLine("The value of a + b is: " + (a + b));
+            //Console.WriteLine("The value of a - b is: " + (a - b));
+            //Console.WriteLine("The value of a * b is: " + (a * b));
+            //Console.WriteLine("The value of a / b is: " + Convert.ToInt32(b / a));
+
+            //int a = Math.Min(34, 343);
+            //int b = Math.Max(34, 343);
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+
+            //double c = Math.Sqrt(36);
+            //Console.WriteLine(c);
+
+            //int d = Math.Abs(-34);
+            //Console.WriteLine(d);
+
+            //string hello = "Hello World!";
+
+            //Console.WriteLine(hello.Length);
+            //Console.WriteLine(hello.ToUpper());
+            //Console.WriteLine(hello.ToLower());
+            //Console.WriteLine(hello + " Okay");
+            //Console.WriteLine(string.Concat(hello, " Okay"));
+
+            //string name = "Suvansh";
+            //int age = 19;
+
+            //Console.WriteLine($"My name is {name} and my age is {age}");
+
+            //Console.WriteLine(name[0]);
+            //Console.WriteLine(hello.IndexOf("World"));
+
+            //Console.Write("Enter your age: ");
+            //string age = Console.ReadLine();
+            //int ageNum = Convert.ToInt32(age);
+
+            //if(ageNum<18)
+            //{
+            //    Console.WriteLine("You are a kid!");    
+            //}
+            //else if (ageNum >= 18 && ageNum < 60)
+            //{
+            //    Console.WriteLine("You are an adult!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("You are an old person!");
+            //}           
+
+            Greet("Suvansh");
+            Greet("Harry");
+
+            Console.WriteLine(Average(2, 13, 4));
 
 
 
